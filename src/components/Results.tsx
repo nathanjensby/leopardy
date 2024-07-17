a2978070f323935bc43ae20af0ca3134df7cbeb7
Nathan Jensby <nathanjensby@Nathans-MacBook-Pro.local>
2024-07-16 22:11:44 -0500

    results: update results content and styles on splash

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,15 +1,34 @@
 import { useContext } from "react";
-import { Button } from "theme-ui";
+import { Button, Flex, Text } from "theme-ui";
 import { PlayersContext } from "../contexts/playersContext";
 import { GameContext } from "../contexts/gameContext";
-import { ACTION_TYPES, GAME_STATE_ACTION_TYPES } from "../types/types";
+import {
+  ACTION_TYPES,
+  ACTIVATED_QUESTIONS_TYPES,
+  GAME_STATE_ACTION_TYPES,
+} from "../types/types";
+import { QuestionsContext } from "../contexts/questionsContext";
 
 const Results = () => {
-  const [, playerDispatch] = useContext(PlayersContext);
+  const [playersState, playerDispatch] = useContext(PlayersContext);
   const [, gameDispatch] = useContext(GameContext);
+  const [, quesitonsDispatch] = useContext(QuestionsContext);
+  const finalPlayersArr = playersState.players.sort(
+    (a, b) => b.score - a.score
+  );
   return (
     <div>
-      <div>Results Page</div>
+      <Text sx={{ fontSize: 36, fontWeight: 700, mb: 4 }} as="h2">
+        Final Standings:
+      </Text>
+      <Flex sx={{ alignItems: "center", flexDirection: "column" }}>
+        {finalPlayersArr.map((player, index) => (
+          <Flex sx={{ width: "50%", justifyContent: "space-between", mb: 4 }}>
+            <Text>{`${index + 1}: ${player.name}`}</Text>
+            <Text>{`$${player.score}`}</Text>
+          </Flex>
+        ))}
+      </Flex>
       <Button
         onClick={() => {
           gameDispatch({
@@ -18,6 +37,9 @@ const Results = () => {
           playerDispatch({
             type: ACTION_TYPES.RESET,
             player: { name: "", id: "", score: 0 },
+          });
+          quesitonsDispatch({
+            type: ACTIVATED_QUESTIONS_TYPES.CLEAR_ALL,
           });
         }}
       >
--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { Box, Button, Input, Label } from "theme-ui";
+import { Box, Button, Flex, Input, Label, Text } from "theme-ui";
 import PlayerList from "./PlayerList";
 import { PlayersContext } from "../contexts/playersContext";
 import { ACTION_TYPES, GAME_STATE_ACTION_TYPES } from "../types/types";
@@ -12,43 +12,61 @@ const Splash = () => {
   const [playerName, setPlayerName] = useState("");
 
   return (
-    <Box>
-      <h1>Leopardy</h1>
-      <Box>
-        <h2>Players</h2>
-        <Label htmlFor="addPlayer">Add player</Label>
-        <Input
-          name="addPlayer"
-          id="addPlayer"
-          mb={3}
-          placeholder="Player 1"
-          value={playerName}
-          onChange={(e) => setPlayerName(e.target.value)}
-        />
+    <Flex
+      sx={{ p: 6, flexDirection: "column", justifyContent: "space-between" }}
+    >
+      <Text
+        as="h1"
+        sx={{
+          mb: 6,
+          fontSize: 128,
+          color: "secondary",
+          fontFamily: "Geopardy",
+        }}
+      >
+        Leopardy!
+      </Text>
+      <Box sx={{ maxWidth: "50%", mb: 4 }}>
+        <Label sx={{ mb: 2 }} htmlFor="addPlayer">
+          Add player
+        </Label>
+        <Flex sx={{ alignItems: "center" }}>
+          <Input
+            name="addPlayer"
+            id="addPlayer"
+            mr={2}
+            placeholder="Player 1"
+            value={playerName}
+            onChange={(e) => setPlayerName(e.target.value)}
+          />
+          <Button
+            sx={{ width: "240px" }}
+            onClick={() => {
+              setPlayerName("");
+              playersDispatch({
+                type: ACTION_TYPES.ADD,
+                player: { name: playerName, score: 0, id: playerName },
+              });
+            }}
+          >
+            Add Player
+          </Button>
+        </Flex>
+      </Box>
+      <PlayerList />
+      <Flex sx={{ justifyContent: "flex-end", mt: 4 }}>
         <Button
-          onClick={() => {
-            setPlayerName("");
-            playersDispatch({
-              type: ACTION_TYPES.ADD,
-              player: { name: playerName, score: 0, id: playerName },
+          onClick={(e) => {
+            e.preventDefault();
+            gameDispatch({
+              type: GAME_STATE_ACTION_TYPES.SINGLE_LEOPARDY,
             });
           }}
         >
-          Add Player
+          Let's go
         </Button>
-      </Box>
-      <PlayerList />
-      <Button
-        onClick={(e) => {
-          e.preventDefault();
-          gameDispatch({
-            type: GAME_STATE_ACTION_TYPES.SINGLE_LEOPARDY,
-          });
-        }}
-      >
-        Let's go
-      </Button>
-    </Box>
+      </Flex>
+    </Flex>
   );
 };
 
