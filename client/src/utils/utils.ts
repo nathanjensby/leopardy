21ae26763e2ca51e3af5f8258a1e5a9000f39b64
Nathan Jensby <nathan.jensby@gmail.com>
2025-01-24 20:57:45 -0600

    websockets: get to work locally and switch back to real urls

--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -1,7 +1,8 @@
-// socket.ts
 import { io, Socket } from "socket.io-client";
 
 // Create a single socket instance
 export const socket: Socket = io("https://leopardybackend.uc.r.appspot.com", {
+  // export const socket: Socket = io("http://localhost:3001", {
   transports: ["websocket", "polling"], // Use WebSocket and fallback to polling
+  reconnection: true,
 });
