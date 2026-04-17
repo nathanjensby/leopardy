40ec9ca5fab48b6fb76b3306d43d29a26e32a0d2
Nathan Jensby <njensby@ea.com>
2026-04-16 20:48:42 -0500

    feat: add host route

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeUIProvider } from "theme-ui";
 import { Routes, Route } from "react-router-dom";
 import BuzzerPage from "./components/BuzzerPage"; // Buzzer component
+import HostKey from "./components/HostKey";
 import { theme } from "./theme";
 import App from "./App";
 import { ToastProvider } from "./contexts/toastProvider";
@@ -12,6 +13,7 @@ const Layout = () => {
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/buzzer" element={<BuzzerPage />} />
+          <Route path="/key" element={<HostKey />} />
         </Routes>
       </ToastProvider>
     </ThemeUIProvider>
